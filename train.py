054670afaf7fdd1b6bd816796960a6838a0d6709
Roos Bottema <ro5122bo-s@student.lu.se>
2023-12-05 08:54:04 +0100

    same same

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         Ll1 = l1_loss(image, gt_image)
         loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
         print(f'type of Ll1: {type(Ll1)}')
-        print(f'Ll1 loss size: {len(Ll1)}')
+        
         print(f'scene size: {scene}')
         print(f'image size> {image.size()}')
         loss.backward()
